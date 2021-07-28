8fbf7feca0cd2df8b1d53166a2f365d073ddefed
AMEMELYANOV <emelyanov.am@gmail.com>
2021-07-28 20:32:51 +0300

    4. Thread без общих ресурсов [#267919 #182778]

--- src/main/java/ru/job4j/ref/UserCache.java
+++ src/main/java/ru/job4j/ref/UserCache.java
@@ -1,6 +1,6 @@
 package ru.job4j.ref;
 
-import net.jcip.annotations.NotThreadSafe;
+import net.jcip.annotations.ThreadSafe;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,7 +8,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 
-@NotThreadSafe
+@ThreadSafe
 public class UserCache {
     private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
     private final AtomicInteger id = new AtomicInteger();
