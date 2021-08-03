e02d259dd02848a252a0fa8bc59963708784c9c1
AMEMELYANOV <emelyanov.am@gmail.com>
2021-08-03 08:52:28 +0300

    3. Класс хранилища пользователей UserStorage [#1104 #184291]

--- src/main/java/ru/job4j/userstorage/UserStore.java
+++ src/main/java/ru/job4j/userstorage/UserStore.java
@@ -12,21 +12,11 @@ public class UserStore {
     final private Map<Integer, User> users = new HashMap<>();
 
     public synchronized boolean add(User user) {
-        boolean rsl = false;
-        if (!users.containsKey(user.getId())) {
-            users.put(user.getId(), new User(user.getId(), user.getAmount()));
-            rsl = true;
-        }
-        return rsl;
+        return users.putIfAbsent(user.getId(), new User(user.getId(), user.getAmount())) == null;
     }
 
     public synchronized boolean update(User user) {
-        boolean rsl = false;
-        if (users.containsKey(user.getId())) {
-            users.replace(user.getId(), new User(user.getId(), user.getAmount()));
-            rsl = true;
-        }
-        return rsl;
+        return users.replace(user.getId(), new User(user.getId(), user.getAmount())) != null;
     }
 
     public synchronized boolean delete(User user) {
@@ -34,18 +24,14 @@ public class UserStore {
     }
 
     public synchronized void transfer(int fromId, int toId, int amount) {
-            User src = users.get(fromId);
-            User dst = users.get(toId);
-            if (src == null) {
-                System.out.println("Source of transfer not found");
-            } else if (dst == null) {
-                System.out.println("Destination of transfer not found");
-            } else if (src.getAmount() < amount) {
-                System.out.println("Not enough money for transfer");
-            } else {
-                src.setAmount(src.getAmount() - amount);
-                dst.setAmount(dst.getAmount() + amount);
-            }
+        User src = users.get(fromId);
+        User dst = users.get(toId);
+        if (src == null || dst == null || src.getAmount() < amount) {
+            System.out.println("Check source, destination or amount");
+        } else {
+            src.setAmount(src.getAmount() - amount);
+            dst.setAmount(dst.getAmount() + amount);
+        }
     }
 
     @Override
@@ -60,11 +46,18 @@ public class UserStore {
 class Test {
     public static void main(String[] args) {
         UserStore storage = new UserStore();
-        storage.add(new User(1, 100));
-        storage.add(new User(2, 200));
+        User user = new User(1, 100);
+        System.out.println(storage.add(user));
+        System.out.println(storage.add(new User(2, 200)));
+        System.out.println(storage.add(new User(2, 200)));
+        user.setAmount(1000);
+        System.out.println(storage);
 
-        storage.update(new User(1, 200));
-        storage.update(new User(2, 400));
+        User replaceUser = new User(2, 400);
+        System.out.println(storage.update(new User(1, 200)));
+        System.out.println(storage.update(replaceUser));
+        System.out.println(storage.update(new User(3, 400)));
+        replaceUser.setAmount(2000);
         System.out.println(storage);
 
         storage.transfer(1, 2, 100);
