a1d040f9a4b68fcf7a088140c567b45283322379
AMEMELYANOV <emelyanov.am@gmail.com>
2021-08-04 09:11:57 +0300

    SimpleBlockingQueue для ParallelSearch

---
+++ src/main/java/ru/job4j/buffer/SimpleBlockingQueue.java
@@ -0,0 +1,53 @@
+package ru.job4j.buffer;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+    @GuardedBy("this")
+    private final Queue<T> queue = new LinkedList<>();
+    private final int limit;
+    private final Object monitor = this;
+
+    public SimpleBlockingQueue(int limit) {
+        this.limit = limit;
+    }
+
+    public void offer(T value) throws InterruptedException {
+        synchronized (monitor) {
+            while (queue.size() == limit) {
+                    monitor.wait();
+             }
+            queue.offer(value);
+            monitor.notifyAll();
+        }
+    }
+
+    public T poll() throws InterruptedException {
+        synchronized (monitor) {
+            while (queue.size() == 0) {
+                    monitor.wait();
+            }
+            T value = queue.poll();
+            monitor.notifyAll();
+            return value;
+        }
+    }
+
+    public int size() {
+        return queue.size();
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public static void main(String[] args) {
+        SimpleBlockingQueue queue = new SimpleBlockingQueue(5);
+        System.out.println(queue.isEmpty());
+    }
+}
