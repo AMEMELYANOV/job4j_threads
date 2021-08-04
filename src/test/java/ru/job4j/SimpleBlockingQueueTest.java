337748f48c70413d18770b9250336bbca3b97bb5
AMEMELYANOV <emelyanov.am@gmail.com>
2021-08-04 08:51:00 +0300

    Небольшие правки

--- src/test/java/ru/job4j/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/SimpleBlockingQueueTest.java
@@ -5,10 +5,10 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class SimpleBlockingQueueTest {
-    SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
 
     @Test
     public void whenFinallyQueueSize0() throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
         Thread producer = new Thread(() -> {
              for (int i = 0; i < 5; i++) {
                 try {
@@ -37,6 +37,7 @@ public class SimpleBlockingQueueTest {
 
     @Test
     public void whenFinallyQueueSizeNot0() throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
         Thread producer = new Thread(() -> {
              for (int i = 0; i < 10; i++) {
                 try {
