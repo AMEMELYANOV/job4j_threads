963a18fb59711f8159f923fd4ace03eb3ae0c515
AMEMELYANOV <emelyanov.am@gmail.com>
2021-08-08 13:36:31 +0300

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/email/EmailNotification.java
@@ -0,0 +1,42 @@
+package ru.job4j.email;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+    private final ExecutorService pool;
+
+    public EmailNotification() {
+        pool = Executors.newFixedThreadPool(
+                Runtime.getRuntime().availableProcessors()
+        );
+    }
+
+    public void emailTo(User user) {
+        String subject = String.format("Notification %s to email %s", user.getUsername(), user.getEmail());
+        String body = String.format("Add a new event to %s", user.getUsername());
+        pool.submit(() -> send(subject, body, user.getEmail()));
+    }
+
+    public void close() {
+        pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void send(String subject, String body, String email) {
+    }
+
+    public static void main(String[] args) {
+        EmailNotification emailNotification = new EmailNotification();
+        for (int i = 0; i < 1000; i++) {
+            emailNotification.emailTo(new User("user" + i, "email" + i));
+        }
+        emailNotification.close();
+    }
+}
